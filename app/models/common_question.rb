1249d859f78ee8ce594331a5d3a6838887ac03f5
Jeffrey Mince <jeffrey.mince@gmail.com>
2015-02-05 16:12:41 -0700

    Added FAQ Page

---
+++ app/models/common_question.rb
@@ -0,0 +1,13 @@
+class CommonQuestion
+  def initialize(question, answer)
+    @question = question
+    @answer = answer
+    @slug = @question.gsub(" ", "-")
+
+end
+
+attr_reader :question
+attr_reader :answer
+attr_reader :slug
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/terms', to: 'terms#index'
   get '/about', to: 'about#index'
+  get '/faq', to: 'common_questions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
